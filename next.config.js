bb3a162c80ef47a457fa07d8fd2dc2bb89f2b2ff
gnehs <jayuiop@gmail.com>
2023-04-26 12:16:23 +0800

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  basePath: '/2023',
+  output: 'export',
+  distDir: 'dist',
 }
-
+console.log('🥞    - url:', "http://localhost:3000/2023/")
 module.exports = nextConfig
