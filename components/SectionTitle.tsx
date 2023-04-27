124a67dc9cf0f93b5f634e66ab4f5f0eae935b79
gnehs <jayuiop@gmail.com>
2023-04-27 15:41:09 +0800

    updated style

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,5 +1,5 @@
 export default function SectionTitle({ children, id }: { children: React.ReactNode, id: string }) {
   return <h1 className="text-4xl font-bold text-[#FFF] text-center my-4 py-4 border-b border-[#FFF] scroll-mt-32" id={id}>
     {children}
-  </ h1>
+  </h1>
 }
