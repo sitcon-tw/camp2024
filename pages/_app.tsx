a4d0c94c0cb84cf3fce0b464667e2f86a58355e2
gnehs <jayuiop@gmail.com>
2023-05-05 00:07:24 +0800

    added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>SITCON 夏令營 2023</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
