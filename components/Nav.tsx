7418a30647a2fab75e8619ce4e79ad054a6bc412
gnehs <jayuiop@gmail.com>
2023-05-05 00:07:10 +0800

    updated nav style

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react"
 import { motion, AnimatePresence, useAnimate, stagger, } from "framer-motion"
-const staggerMenuItems = stagger(0.1, { startDelay: 0.1 });
 
+function NavItem({ href, children, setIsNavOpen }: {
+  href: string,
+  children: React.ReactNode,
+  setIsNavOpen: React.Dispatch<React.SetStateAction<boolean>>
+}) {
+  return <motion.a href={href} className="text-white hover:text-[#F9A8D4] font-bold" onClick={() => setIsNavOpen(false)}>
+    {children}
+  </motion.a>
+}
 function useMenuAnimation(isOpen: boolean) {
+  const staggerMenuItems = stagger(0.1, { startDelay: 0.05 });
   const [scope, animate] = useAnimate();
 
   useEffect(() => {
@@ -25,10 +34,12 @@ function useMenuAnimation(isOpen: boolean) {
     animate(
       "#mobile-nav a",
       isOpen
-        ? { opacity: 1, scale: 1, x: 0, filter: "blur(0px)" }
-        : { opacity: 0, scale: 0.4, x: 20, filter: "blur(20px)" },
+        ? { opacity: 1, y: 0, filter: 'blur(0px)' }
+        : { opacity: 0, y: -100, filter: 'blur(10px)' },
       {
-        duration: 0.3,
+        type: "spring",
+        bounce: 10,
+        duration: 0.2,
         delay: isOpen ? staggerMenuItems : 0
       }
     );
@@ -38,12 +49,6 @@ function useMenuAnimation(isOpen: boolean) {
 }
 export default function Nav() {
   const [isNavOpen, setIsNavOpen] = useState(false)
-  function NavItem({ href, children }: {
-    href: string,
-    children: React.ReactNode
-  }) {
-    return <a href={href} className="text-white hover:text-[#F9A8D4] font-bold" onClick={() => setIsNavOpen(false)}>{children}</a>
-  }
   const scope = useMenuAnimation(isNavOpen);
   const navItems = [
     { href: '#application-info', text: '報名資訊' },
@@ -66,10 +71,8 @@ export default function Nav() {
             <button onClick={() => setIsNavOpen(!isNavOpen)} className="text-white hover:text-[#F9A8D4] font-bold">
               {isNavOpen ? '關閉' : '選單'}
             </button>
-
-
-            <motion.div
-              className=" max-w-[200px] w-[80vw] absolute top-6 -right-4 m-auto bg-[#7A44AF] bg-opacity-80 backdrop-blur-xl rounded-2xl  mt-8 shadow-2xl lg:hidden overflow-hidden z-50"
+            <div
+              className="w-[200px] max-w-[80vw] absolute top-6 -right-4 m-auto bg-[#7A44AF] bg-opacity-80 backdrop-blur-xl rounded-2xl  mt-8 shadow-2xl lg:hidden overflow-hidden z-50"
               style={{
                 pointerEvents: isNavOpen ? "auto" : "none",
                 clipPath: "inset(5% 5% 95% 95% round 10px)"
@@ -77,9 +80,9 @@ export default function Nav() {
               id="mobile-nav"
             >
               <div className="py-4 px-6 flex flex-col justify-between space-y-4">
-                {navItems.map((item, index) => <NavItem href={item.href} key={index}>{item.text}</NavItem>)}
+                {navItems.map((item, index) => <NavItem href={item.href} key={index} setIsNavOpen={setIsNavOpen}>{item.text}</NavItem>)}
               </div>
-            </motion.div>
+            </div>
           </div>
         </nav>
       </div>
