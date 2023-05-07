999318997480cfeaeb43e52c239d92177695cb23
gnehs <jayuiop@gmail.com>
2023-05-07 22:18:56 +0800

    updated visual motion

--- components/Visual.tsx
+++ components/Visual.tsx
@@ -21,14 +21,18 @@ export default function Visual() {
     stiffness: 400
   })
   const vw = width / 100
+  const logoY = useTransform(springScrollY, [0, 1], [0, -70]);
   const catX = useTransform(springScrollY, [0, 1], [0, -10 * vw]);
   const catY = useTransform(springScrollY, [0, 1], [0, 25]);
   const astronautX = useTransform(springScrollY, [0, 1], [0, 10 * vw]);
   const astronautY = useTransform(springScrollY, [0, 1], [0, 50]);
   const planetY = useTransform(warpTime, [0, 4000, 8000], [0, 25, 0], { ease: easeInOut })
+  const infoY = useTransform(warpScrollY, [0, 1], [0, -70]);
   return <div className="w-full h-[100vh]  overflow-hidden " ref={visualRef}>
     <div className="container relative">
-      <img className="absolute top-[12.5vh] w-40 md:w-60 left-0 right-0 m-auto" src="/2023/visual/logo.svg" alt="logo" />
+      <motion.img
+        style={{ y: logoY }}
+        className="absolute top-[12.5vh] w-40 md:w-60 left-0 right-0 m-auto" src="/2023/visual/logo.svg" alt="logo" />
       <motion.img
         style={{ x: catX, y: catY }}
         className="absolute top-[35vh] w-[150px] sm:w-[180px] md:w-[300px] right-0 m-auto" src="/2023/visual/cat.svg" alt="cat" />
@@ -38,7 +42,9 @@ export default function Visual() {
       <motion.img
         style={{ y: planetY }}
         className="absolute top-[45vh] w-[250px] sm:w-[450px] md:w-[400px] left-0 right-0 m-auto" src="/2023/visual/planet.svg" alt="planet" />
-      <motion.div className="absolute top-[75vh] m-auto w-full flex flex-col items-start gap-1 lg:gap-2 text-center text-white leading-4">
+      <motion.div
+        style={{ y: infoY }}
+        className="absolute top-[75vh] m-auto w-full flex flex-col items-start gap-1 lg:gap-2 text-center text-white leading-4">
         <div className="lg:text-xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">日期</div>
         <div className="text-2xl lg:text-4xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">07/20 ~ 07/24</div>
         <div className="lg:text-xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded mt-2 p-1">地點</div>
