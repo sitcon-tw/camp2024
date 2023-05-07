db65863c8c02f6f9cc2ad54b5a7ed4bc2637862d
gnehs <jayuiop@gmail.com>
2023-05-07 13:21:31 +0800

    added membersMD5.js

---
+++ utils/membersMD5.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const md5 = require('md5');
+let result = [];
+fs.readFile('./members.csv', 'utf8', (err, data) => {
+  if (err) throw err;
+  let members = data.split('\n');
+  for (let i = 1; i < members.length; i++) {
+    let member = members[i].split(',');
+    result.push({
+      group: member[0],
+      name: member[1],
+      md5: md5(member[2].trim().toLowerCase())
+    });
+  }
+  fs.writeFile('./membersMD5.json', JSON.stringify(result), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+})
