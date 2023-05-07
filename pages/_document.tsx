34c338b5f16c2f72c74a19ac3700789b7cc1e9f9
yc97463 <hi@imych.one>
2023-05-07 15:27:02 +0800

    feat(p/_doc): add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="zh-TW" className='scroll-smooth'>
+    <Html lang="zh-TW" className="scroll-smooth">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link data-n-head="1" rel="icon" href="/2023/visual/planet.svg" />
       </Head>
-      <body className='bg-[#012637] text-white leading-7'>
+      <body className="bg-[#012637] text-white leading-7">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
