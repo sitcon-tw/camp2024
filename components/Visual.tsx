0ae95253693ce47cd3ca98dbec886774996e993b
gnehs <jayuiop@gmail.com>
2023-05-08 19:43:57 +0800

    added registration to visual

--- components/Visual.tsx
+++ components/Visual.tsx
@@ -26,6 +26,11 @@ export default function Visual() {
   const astronautY = useTransform(springScrollY, [0, 1], [0, 50]);
   const planetY = useTransform(warpTime, [0, 4000, 8000], [0, 25, 0], { ease: easeInOut })
   const infoY = useTransform(warpScrollY, [0, 1], [0, -70]);
+  const dateOpacity = useTransform(warpTime, [0, 2000, 6000, 8000], [1, 0, 0, 1]);
+  const dateHeight = useTransform(warpTime, [0, 2000, 6000, 8000], ["auto", 0, 0, "auto"]);
+  const date2Opacity = useTransform(warpTime, [0, 2000, 6000, 8000], [0, 1, 1, 0]);
+  const date2Height = useTransform(warpTime, [0, 2000, 6000, 8000], [0, "auto", "auto", 0]);
+
   return <div className="w-full h-[100vh]  overflow-hidden " ref={visualRef}>
     <div className="container relative">
       <motion.img
@@ -42,11 +47,28 @@ export default function Visual() {
         className="absolute top-[45vh] w-[250px] sm:w-[450px] md:w-[400px] left-0 right-0 m-auto" src="/2023/visual/planet.svg" alt="planet" />
       <motion.div
         style={{ y: infoY }}
-        className="absolute top-[75vh] m-auto w-full flex flex-col items-start gap-1 lg:gap-2 text-center text-white leading-4">
-        <div className="lg:text-xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">日期</div>
-        <div className="text-2xl font-bold lg:text-4xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">07/20 ~ 07/24</div>
+        className="absolute top-[75vh] m-auto w-full flex flex-col items-start gap-1 lg:gap-2 text-white leading-4">
         <div className="lg:text-xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded mt-2 p-1">地點</div>
         <div className="text-2xl font-bold lg:text-4xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">陽明交通大學 光復校區</div>
+        <div className="flex gap-8">
+          <div>
+
+            <motion.div className="lg:text-xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">
+              日期
+            </motion.div>
+            <motion.div className="text-2xl font-bold lg:text-4xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">
+              07/20 ~ 07/24
+            </motion.div>
+          </div>
+          <div>
+            <motion.div className="lg:text-xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">
+              報名期限
+            </motion.div>
+            <motion.div className="text-2xl font-bold lg:text-4xl bg-[#012637] bg-opacity-80 backdrop-blur-xl rounded p-1">
+              即日起 ~ 05/26
+            </motion.div>
+          </div>
+        </div>
       </motion.div>
     </div>
   </div >
