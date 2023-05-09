1dd89de5bae2411d942e56985307b7270468650f
gnehs <jayuiop@gmail.com>
2023-05-10 00:50:50 +0800

    changed visual order

--- components/Visual.tsx
+++ components/Visual.tsx
@@ -41,6 +41,10 @@ export default function Visual() {
         dragElastic={0.2}
         style={{ y: logoY }}
         className="absolute top-[12.5vh] w-40 md:w-60 left-0 right-0 m-auto cursor-grab" src="/2023/visual/logo.svg" alt="logo" />
+      <Planets
+        style={{ y: planetY }}
+        className="absolute top-[35vh] w-[250px] sm:w-[450px] md:w-[400px] left-0 right-0 m-auto"
+      />
       <motion.img
         drag
         whileDrag={{ scale: 1.2, rotate: -5 }}
@@ -63,10 +67,6 @@ export default function Visual() {
         }}
         style={{ x: astronautX, y: astronautY }}
         className="absolute top-[35vh] w-[160px] sm:w-[200px] md:w-[320px] left-0 m-auto cursor-grab" src="/2023/visual/astronaut.svg" alt="astronaut" />
-      <Planets
-        style={{ y: planetY }}
-        className="absolute top-[35vh] w-[250px] sm:w-[450px] md:w-[400px] left-0 right-0 m-auto"
-      />
       <motion.div
         style={{ y: infoY }}
         className="absolute top-[75vh] m-auto w-full flex flex-col items-start gap-1 lg:gap-2 text-white leading-4">
