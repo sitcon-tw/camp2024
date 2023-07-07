a617de8eee9c604dea1e07076c59ccde0bd571fd
yc97463 <hi@imych.one>
2023-07-07 19:48:06 +0800

    feat(components/TimeTable): eslint-disable

--- components/TimeTable.tsx
+++ components/TimeTable.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import schedule from "../public/schedule.json";
 import { useEffect, useState } from "react";
 import { motion, useMotionValue, useSpring } from "framer-motion";
