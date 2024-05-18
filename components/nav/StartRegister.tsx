3175bdfce6ec52365f10a959c0209428957be1d0
banahaker <pn0818x@outlook.com>
2024-05-19 00:42:01 +0800

    feat: Add StartRegister component to Nav

---
+++ components/nav/StartRegister.tsx
@@ -0,0 +1,10 @@
+export default function StartRegister() {
+  return (
+    <a
+      className="bg-gradient-to-b from-[#78C2FF] to-[#9C7DFF] rounded-full p-1"
+      href="#"
+    >
+      <p className="bg-white text-md font-bold text-[#1E3D6C] px-6 py-1 rounded-full h-full">開始報名</p>
+    </a>
+  );
+}
