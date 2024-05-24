d85466984b7a56cb9f4e1faaa332d8cfdaf953ae
banahaker <pn0818x@outlook.com>
2024-05-24 20:26:08 +0800

    feat: Update Intro and Visual components
    - Updated Intro component with new styling and content.
    - Updated Visual component with new styling and added activity date and location information.

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,18 +2,18 @@ import { motion } from "framer-motion";
 export default function Intro() {
   return (
     <>
-      <div className="bg-[#0C1420] container p-8">
+      <div className="bg-[#0C1420] container p-8 pt-28">
         <motion.h2
           initial={{ opacity: 0, y: 10 }}
           whileInView={{ opacity: 1, y: 0 }}
-          className="font-medium text-sm"
+          className="font-medium text-md"
         >
           樂趣之餘，不忘學習
         </motion.h2>
         <motion.h1
           initial={{ opacity: 0, y: 10 }}
           whileInView={{ opacity: 1, y: 0 }}
-          className="text-md font-bold"
+          className="text-lg font-bold"
         >
           由資訊社群所打造，超高知識含量的夏令營
         </motion.h1>
@@ -21,15 +21,21 @@ export default function Intro() {
           <motion.p
             initial={{ opacity: 0, y: 10 }}
             whileInView={{ opacity: 1, y: 0 }}
-            className="text-xs font-thin tracking-wider leading-relaxed"
+            className="text-sm font-thin tracking-wider leading-relaxed"
           >
-            有鑑於資訊領域逐漸成為科技時代的重要學門，社群與開源精神亦持續抬頭，SITCON 團隊認為將資訊教育向下扎根，為在資訊科學領域有興趣的學生提供一個親切而良好的機會入門，並播灑開源種子，是一件相當重要且值得去做的事情。
+            有鑑於資訊領域逐漸成為科技時代的重要學門，社群與開源精神亦持續抬頭，SITCON
+            團隊認為將資訊教育向下扎根，為在資訊科學領域有興趣的學生提供一個親切而良好的機會入門，並播灑開源種子，是一件相當重要且值得去做的事情。
             <br />
             <br />
-            2014 年至今，SITCON 夏令營已將邁入第八屆，在此之前營隊的課程內容涵蓋開放原始碼、資訊安全、Maker、Python、後端的 Node.js、前端的資料爬蟲、視覺化、Telegram bot 等。另外更有社群闖關、視界咖啡館、黑客松等活動，讓學員充分吸收新知、動手實作、尋找自己的愛好及興趣，進而加入我們一起推動 Open Source。
+            2014 年至今，SITCON
+            夏令營已將邁入第八屆，在此之前營隊的課程內容涵蓋開放原始碼、資訊安全、Maker、Python、後端的
+            Node.js、前端的資料爬蟲、視覺化、Telegram bot
+            等。另外更有社群闖關、視界咖啡館、黑客松等活動，讓學員充分吸收新知、動手實作、尋找自己的愛好及興趣，進而加入我們一起推動
+            Open Source。
             <br />
             <br />
-            今年 SITCON 夏令營將於陽明交通大學光復校區舉辦，以「拉麵加點 source，麵向開源界」為主軸。正如同拉麵的麵條和配料可以隨個人喜好自由搭配組合，在程式設計中也可以讓每個人根據自己的想法和需求，選用不同的程式模組和元件，打造獨一無二的作品。透過兩者結合，傳達開源及資訊的無限可能性，在動手實作的過程中，激發創意靈感，發掘個人興趣所在，進而體會開源的樂趣與價值。
+            今年 SITCON 夏令營將於陽明交通大學光復校區舉辦，以「拉麵加點
+            source，麵向開源界」為主軸。正如同拉麵的麵條和配料可以隨個人喜好自由搭配組合，在程式設計中也可以讓每個人根據自己的想法和需求，選用不同的程式模組和元件，打造獨一無二的作品。透過兩者結合，傳達開源及資訊的無限可能性，在動手實作的過程中，激發創意靈感，發掘個人興趣所在，進而體會開源的樂趣與價值。
           </motion.p>
           <img
             src="/2024/images/48711198528_e7ac02a80b_k.jpg"
--- components/Visual.tsx
+++ components/Visual.tsx
@@ -1,14 +1,53 @@
-import Image from "next/image";
-
 export default function Visual() {
   return (
-    <div  className="bg-[#01405D] bg-[url('/2024/visual/visual-background.png')]  bg-center bg-cover h-[65dvh] flex flex-col px-4 pt-20 pb-20">
-      <div className="title self-end flex flex-col items-end" >
-        <p className="font-bold text-3xl drop-shadow-2xl bg-clip-text">學生計算機年會 <img src="/2024/camp-text.svg" alt="camp" className="inline-block" /></p>
+    <div className="bg-[#01405D] bg-[url('/2024/visual/visual-background.png')] bg-center bg-cover h-[65dvh] flex flex-col px-4 pt-20 pb-24 items-stretch box-border relative">
+      <div className="title self-end flex flex-col items-end">
+        <p className="font-bold text-3xl drop-shadow-2xl bg-clip-text">
+          學生計算機年會{" "}
+          <img src="/2024/camp-text.svg" alt="camp" className="inline-block" />
+        </p>
         <p>2024 sitcon camp</p>
       </div>
-      <div className="picture self-start">
-        <Image src="/2024/visual/cat-and-text.png" alt="cat and text" width={340} height={340}></Image>
+      <div className="self-start flex-grow box-border overflow-hidden">
+        <img
+          src="/2024/visual/cat-and-text.png"
+          alt="cat and text"
+          className="max-h-full w-auto"
+        />
+      </div>
+      <div className="absolute -bottom-[5.5rem] left-[50vw] -translate-x-1/2 w-[calc(100%-4rem)] ">
+        <div className="bg-gradient-to-br from-[#2E4B7680] to-[#060A10] backdrop-blur-sm relative h-[11rem] min-w-[20rem] w-[45dvw] rounded-tr-[4rem] rounded-l-2xl rounded-br-2xl mx-auto">
+          <div className="w-full h-full">
+            <div className="flex flex-col p-4">
+              <div className="flex gap-2 items-center">
+                <img
+                  src="/2024/visual/clock-for-activity-date.png"
+                  alt="clock"
+                  className="w-6 h-6"
+                />
+                <p className="font-medium text-sm text-[#EAD377] -translate-y-1">
+                  活動日期
+                </p>
+              </div>
+              <div className=" pl-8 font-medium text-xl">07/17-07/21</div>
+            </div>
+            <div className="flex flex-col p-4">
+              <div className="flex gap-2 items-center">
+                <img
+                  src="/2024/visual/map-marker-for-location.png"
+                  alt="clock"
+                  className="w-6 h-6"
+                />
+                <p className="font-medium text-sm text-[#EAD377] -translate-y-1">
+                  活動地點
+                </p>
+              </div>
+              <div className=" pl-8 font-medium text-xl">
+                陽明交通大學光復校區
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
