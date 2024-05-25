a072db154c181185e3ca49f94c6158aadd7e92b5
HACO <haco.tw@gmail.com>
2024-05-25 20:06:57 +0800

    fix: camp text

--- components/Visual.tsx
+++ components/Visual.tsx
@@ -1,25 +1,28 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Visual() {
   return (
     <div className="bg-[#01405D] bg-[url('/2024/visual/visual-background.png')] bg-center bg-cover h-[65dvh] flex flex-col px-4 pt-20 pb-24 items-stretch box-border relative">
-      <div className="title self-end flex flex-col items-end">
-        <p className="font-bold text-3xl drop-shadow-2xl bg-clip-text">
-          學生計算機年會{" "}
-          <img src="/2024/camp-text.svg" alt="camp" className="inline-block" />
+      <div className="flex flex-col items-end self-end title">
+        <p className="text-3xl font-bold drop-shadow-2xl bg-clip-text">
+          學生計算機年會<span className={"camp-text " + inter.className}>夏令營</span>
         </p>
         <p>2024 sitcon camp</p>
       </div>
-      <div className="self-start flex-grow box-border overflow-hidden">
+      <div className="box-border self-start flex-grow overflow-hidden">
         <img
           src="/2024/visual/cat-and-text.png"
           alt="cat and text"
-          className="max-h-full w-auto"
+          className="w-auto max-h-full"
         />
       </div>
       <div className="absolute -bottom-[5.5rem] left-[50vw] -translate-x-1/2 w-[calc(100%-4rem)] ">
         <div className="bg-gradient-to-br from-[#2E4B7680] to-[#060A10] backdrop-blur-sm relative h-[11rem] min-w-[20rem] w-[45dvw] rounded-tr-[4rem] rounded-l-2xl rounded-br-2xl mx-auto">
           <div className="w-full h-full">
             <div className="flex flex-col p-4">
-              <div className="flex gap-2 items-center">
+              <div className="flex items-center gap-2">
                 <img
                   src="/2024/visual/clock-for-activity-date.png"
                   alt="clock"
@@ -29,10 +32,10 @@ export default function Visual() {
                   活動日期
                 </p>
               </div>
-              <div className=" pl-8 font-medium text-xl">07/17-07/21</div>
+              <div className="pl-8 text-xl font-medium ">07/17-07/21</div>
             </div>
             <div className="flex flex-col p-4">
-              <div className="flex gap-2 items-center">
+              <div className="flex items-center gap-2">
                 <img
                   src="/2024/visual/map-marker-for-location.png"
                   alt="clock"
@@ -42,7 +45,7 @@ export default function Visual() {
                   活動地點
                 </p>
               </div>
-              <div className=" pl-8 font-medium text-xl">
+              <div className="pl-8 text-xl font-medium ">
                 陽明交通大學光復校區
               </div>
             </div>
